2e45edf03717d4b3a64a0477b2f3c4f14124cfcd
Nerry <Nerry@DESKTOP-MNQIHIJ>
2022-08-03 23:11:34 -0600

    nuevo cambio

--- tarea1.1/src/claseprincipal.java
+++ tarea1.1/src/claseprincipal.java
@@ -3,7 +3,7 @@ public class claseprincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" hola mundo");
+		System.out.println(" hola mundo nery");
 
 	}
 
